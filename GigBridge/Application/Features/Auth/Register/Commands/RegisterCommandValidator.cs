151c15f2a72e7d29e89a35c899e0ea1c8b324643
meomeoQuan <QUANNADE180924@fpt.edu.vn>
2026-05-30 13:01:29 +0700

    fix rule for role in registercommanhandle

--- GigBridge/Application/Features/Auth/Register/Commands/RegisterCommandValidator.cs
+++ GigBridge/Application/Features/Auth/Register/Commands/RegisterCommandValidator.cs
@@ -30,8 +30,7 @@ namespace Application.Features.Auth.Register.Commands
                 .WithMessage("Passwords do not match.");
 
             RuleFor(v => v.RegisterRequest.role)
-                .GreaterThan(0)
-                .WithMessage("Role is required.");
+               .NotEmpty().WithMessage("Role is required.");
         }
     }
 }
