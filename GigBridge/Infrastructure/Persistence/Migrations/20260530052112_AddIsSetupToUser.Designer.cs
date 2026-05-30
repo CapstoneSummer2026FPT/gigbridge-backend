ced5ab7319f4d956cfb19eae860985ada0299419
Đoàn Nam Sơn <namson1821@gmail.com>
2026-05-30 12:24:10 +0700

    Update isSetup to Backend

--- GigBridge/Infrastructure/Persistence/Migrations/20260530052112_AddIsSetupToUser.Designer.cs
+++ GigBridge/Infrastructure/Persistence/Migrations/20260530052112_AddIsSetupToUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(GigbridgeDbContext))]
-    partial class GigbridgeDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260530052112_AddIsSetupToUser")]
+    partial class AddIsSetupToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
