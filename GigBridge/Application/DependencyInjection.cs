7e994627437743eb5cf221d83475b52edb71495c
tyn7624 <de180524voxuanthanh@gmail.com>
2026-06-24 04:52:35 +0700

    add

--- GigBridge/Application/DependencyInjection.cs
+++ GigBridge/Application/DependencyInjection.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+
 namespace Application;
 
 public static class DependencyInjection
@@ -32,13 +33,14 @@ public static class DependencyInjection
         services.AddAutoMapper(cfg => {
             cfg.LicenseKey = configuration["AutoMapper:LicenseKey"];
         }, typeof(MappingProfile));
-
+        services.AddScoped<ScheduleWorkflowService>();
         services.AddScoped<IUserEloService, UserEloService>();
         services.AddScoped<ScheduleWorkflowService>();
 
         services.AddSingleton<DeadlineWarningService>();
         services.AddSingleton<IDeadlineWarningService>(sp => sp.GetRequiredService<DeadlineWarningService>());
         services.AddSingleton<IHostedService>(sp => sp.GetRequiredService<DeadlineWarningService>());
+
 
         services.AddSingleton<DeliveryOutboxService>();
         services.AddSingleton<IHostedService>(sp => sp.GetRequiredService<DeliveryOutboxService>());
