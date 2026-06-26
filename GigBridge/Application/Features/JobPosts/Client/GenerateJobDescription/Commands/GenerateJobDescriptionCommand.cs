a1d9cf928111210e37f8552f7f26cee59820add6
meomeoQuan <QUANNADE180924@fpt.edu.vn>
2026-06-26 16:25:38 +0700

    modify input from list of question -> user prompt , modify output adding vetting questions to assess freelancer

--- GigBridge/Application/Features/JobPosts/Client/GenerateJobDescription/Commands/GenerateJobDescriptionCommand.cs
+++ GigBridge/Application/Features/JobPosts/Client/GenerateJobDescription/Commands/GenerateJobDescriptionCommand.cs
@@ -3,4 +3,4 @@ using Application.Features.JobPosts.Client.GenerateJobDescription.DTOs;
 
 namespace Application.Features.JobPosts.Client.GenerateJobDescription.Commands;
 
-public record GenerateJobDescriptionCommand(List<string> VettingQuestions) : IRequest<GenerateJobDescriptionResponse>;
+public record GenerateJobDescriptionCommand(string ClientPrompt) : IRequest<GenerateJobDescriptionResponse>;
